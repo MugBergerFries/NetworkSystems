4c99bbfd76d7eb0d4e9089406119395741967284
MugBergerFries <sjamesberger@gmail.com>
2018-09-21 15:17:16 -0600

    a

--- Assignment1/udp_client.c
+++ Assignment1/udp_client.c
@@ -107,7 +107,7 @@ int main(int argc, char **argv) {
 	        printf("INPUTTED FILENAME: %s\n", fname);
  			curfile = fopen(fname, "r+");
  			if (curfile!=NULL){
- 				fseek(curfile, 0, SEEK_END);
+ 				fseek(curfile, 1, SEEK_END);
  				printf("AHHHHH: %d\n", ftell(curfile));
 	        	if (fseek(curfile, 0, SEEK_END) != 0) printf("ERROR IN FSEEK\n");
 		        filesize = ftell(curfile);
