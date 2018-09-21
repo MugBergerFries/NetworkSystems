0d93ba76575199937a9a5c6fa2ad35fa91cce7c0
MugBergerFries <sjamesberger@gmail.com>
2018-09-21 15:41:06 -0600

    asd

--- Assignment1/udp_client.c
+++ Assignment1/udp_client.c
@@ -117,6 +117,7 @@ int main(int argc, char **argv) {
 		        n = sendto(sockfd, sizebuf, 4096, 0, &serveraddr, serverlen);
 		        char tempbuf[filesize];
 		        fread(tempbuf, 1, filesize, curfile);
+		        printf("%s\n", tempbuf);
 		        n = sendto(sockfd, tempbuf, strlen(tempbuf), 0, &serveraddr, serverlen);
 	    	}
 	    	else{
