f0a35e19eb301e22a36a027bd9a811c01244d953
MugBergerFries <sjamesberger@gmail.com>
2018-09-21 14:02:07 -0600

    Bugfixes

--- Assignment1/udp_server.c
+++ Assignment1/udp_server.c
@@ -127,10 +127,10 @@ int main(int argc, char **argv) {
           filesize = ftell(curfile);
           rewind(curfile);
           char sizebuf[4096];
-          sprintf(sizebuf, "%d", filesize)
+          sprintf(sizebuf, "%d", filesize);
           n = sendto(sockfd, sizebuf, 4096, 0, &serveraddr, serverlen);
           char tempbuf[filesize];
-          fread(tempbuf, 1, filesize, fd);
+          fread(tempbuf, 1, filesize, curfile);
           n = sendto(sockfd, tempbuf, strlen(tempbuf), 0, &serveraddr, serverlen);
         }
         else if (i==2){
