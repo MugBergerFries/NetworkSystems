0f0203aab80c9e543df797d19be5e8baa5fc4547
MugBergerFries <sjamesberger@gmail.com>
2018-10-16 13:40:01 -0600

    Set up the basis for the server, including the ability to handle multiple users

--- Assignment2/server.c
+++ Assignment2/server.c
@@ -5,7 +5,42 @@
 #include<unistd.h>
 
 int main(int argc, char *argv[]){
-	for (;;){
-		printf("%s", "Newline\n");
+	struct sockaddr_in server, client;
+	int sock;
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(argv[1]);
+	if(bind(sock, (struct sockaddr*)&server, sizeof(server))==-1){
+		perror("ERROR: Bind failed");
+		return 1;
+	}
+	puts("SUCCESS: Bind complete");
+	if (!!listen(sock,100)){
+		perror("ERROR: Listen failed");
+		return 1;
+	}
+	puts("SUCCESS: Listen complete");
+	int pid = -1;
+	int accepted, wstatus;
+	while {
+		accepted = accept(sock, (struct sockaddr*)&client, sizeof(client));
+		if (accepted<0){
+			perror("ERROR: Accept failed");
+			return 1;
+		}
+		puts("SUCCESS: Accept complete");
+		pid=fork();
+		if (pid==-1){
+			perror("ERROR: Fork failed");
+			return 1;
+		}
+		puts("SUCCESS: Fork complete");
+		if (pid>0){ //We're the parent process, pid is child's pid
+			waitpid(-1, &wstatus, WNOHANG);//Check if any process has finished, but don't hang
+		}
+		else{ //We're the child process
+			
+		}
 	}
 }
