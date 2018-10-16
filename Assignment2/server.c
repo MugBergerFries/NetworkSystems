91a6876b3f39f8a68106b75429f816a6cb9448bf
MugBergerFries <sjamesberger@gmail.com>
2018-10-16 12:33:45 -0600

    Created server file

---
+++ Assignment2/server.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[]){
+	for (;;){
+		printf("%s". "Newline\n");
+	}
+}
