50faaa660651a6ec824eb0909d38623953640141
MugBergerFries <sjamesberger@gmail.com>
2018-11-15 09:33:23 -0700

    Server socket implementation

--- Assignment3/proxy.cpp
+++ Assignment3/proxy.cpp
@@ -14,12 +14,18 @@ using namespace std;
 int main(int argc, char* argv[]){
 	int port; //webproxy port
 	int ssock, csock; //server and client socket
-	struct sockaddr_in server, client;
-	if (argc != 2) {
+	struct sockaddr_in server, client;//Endpoint addresses for client and server
+	if (argc != 2) {//Make sure there are the right amount of inputs
 		cout<<"Usage: ./webproxy <port>"<<endl;
 		exit(0);
 	}
-	port=atoi(argv[1]);
-	cout<<port<<endl;
-	return 1;
+	port=atoi(argv[1]); //does not validate input
+	ssock=socket(AF_INET, SOCK_STREAM, 0);//Create an IPv4 TCP socket with no flags
+	server.sin_family = AF_INET;//IPv4
+	server.sin_addr.s_addr = INADDR_ANY;//Bind to all interfaces
+	server.sin_port = htons(port);//Set server port as argv[1]
+	if(bind(ssock, (struct sockaddr*)&server, sizeof(server))==-1){//Bind server to sock
+		perror("ERROR: Bind failed");
+		return 1;
+	}
 }
