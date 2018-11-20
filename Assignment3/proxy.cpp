42f394da9bf0acc9c46998e8f8de80d08e111e46
MugBergerFries <sjamesberger@gmail.com>
2018-11-19 17:52:25 -0700

    working on implementing cache and forwarding

--- Assignment3/proxy.cpp
+++ Assignment3/proxy.cpp
@@ -9,45 +9,41 @@
 #include<sys/wait.h>
 #include<stdlib.h>
 #include<tr1/functional>
+#include<algorithm>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
-	string a = "testing";
-	string b = "testing";
-	string c = "tester";
 	hash<string> f;
-	size_t ah = f(a);
-	size_t bh = f(b);
-	size_t ch = f(c);
-	cout<<ah<<" - "<<bh<<" - "<<ch<<endl;
-	/*int port, socksize; //webproxy port
-	int ssock, csock; //server and client socket
-	struct sockaddr_in server, client;//Endpoint addresses for client and server
+	int port, socksize; //webproxy port
+	int psock, csock; //proxy and client socket
+	FILE* curfile;
+	vector<vector<string>> urlcache;
+	struct sockaddr_in proxy, client;//Endpoint addresses for client and proxy
 	if (argc != 3) {//Make sure there are the right amount of inputs
 		cout<<"Usage: ./webproxy <port> <timeout>"<<endl;
 		exit(0);
 	}
 	port=atoi(argv[1]); //does not validate input
-	ssock=socket(AF_INET, SOCK_STREAM, 0);//Create an IPv4 TCP socket with no flags
-	server.sin_family = AF_INET;//IPv4
-	server.sin_addr.s_addr = INADDR_ANY;//Bind to all interfaces
-	server.sin_port = htons(port);//Set server port as argv[1]
-	if(bind(ssock, (struct sockaddr*)&server, sizeof(server))<0){//Bind server to sock
+	psock=socket(AF_INET, SOCK_STREAM, 0);//Create an IPv4 TCP socket with no flags
+	proxy.sin_family = AF_INET;//IPv4
+	proxy.sin_addr.s_addr = INADDR_ANY;//Bind to all interfaces
+	proxy.sin_port = htons(port);//Set proxy port as argv[1]
+	if(bind(psock, (struct sockaddr*)&proxy, sizeof(proxy))<0){//Bind proxy to sock
 		perror("ERROR: Bind failed");
-		close(ssock);
+		close(psock);
 		exit(0);
 	}
-	if (!!listen(ssock,100)){//Set sock as a passive socket with a max queue of 100 requests
+	if (!!listen(psock,100)){//Set sock as a passive socket with a max queue of 100 requests
 		perror("ERROR: Listen failed");
-		close(ssock);
+		close(psock);
 		exit(0);
 	}
 	int pid = -1;//Process id for future children
 	int wstatus;
 	while (1){
 		socksize = sizeof(client);
-		csock = accept(ssock, (struct sockaddr*)&client, (socklen_t*)&socksize);//Accept an incoming connetion (SYN, ACK)
+		csock = accept(psock, (struct sockaddr*)&client, (socklen_t*)&socksize);//Accept an incoming connetion (SYN, ACK)
 		if (csock<0){
 			perror("ERROR: Accept failed");
 			return 1;
@@ -67,11 +63,11 @@ int main(int argc, char* argv[]){
 			}
 		}
 		else{ //We're the child process
-			close(ssock);//Close the parent's connection
+			close(psock);//Close the parent's connection
 			messagein = (char*)malloc(100000);//Allocate memory for the incoming message
 			recv(csock, messagein, 100000, 0);//Receive the message
 			cout<<"\nRECEIVED MESSAGE BEGIN\n\n"<<messagein<<"RECEIVED MESSAGE END\n\n";
-			char * med;//Middleman to parse message
+			char* med;//Middleman to parse message
 			med = strtok(messagein, " ");//Get message until first " "
 			string method, path, vers;
 			method = med;//In this case, this will be GET, POST, HEAD
@@ -79,7 +75,133 @@ int main(int argc, char* argv[]){
 			path = med;//In this case, this will be the path requested
 			med = strtok(NULL, " ");
 			vers = med;//This will be the HTTP version
+			urlindex;
+			urlhash = hash(path);
 			if (method=="GET"){
+				if(urlindex = find(urlcache.begin(), urlcache.end(), urlhash) != urlcache.end()) {
+					curfile = fopen("cache/"+urlhash, "rb");
+					if (fseek(curfile, 0, SEEK_END) != 0) perror("ERROR: Fseek failed");
+					filesize = ftello(curfile);//Seek to end of file and report position to get file size
+					rewind(curfile);//Reset file location pointer
+					char* filedata = (char*)malloc(filesize);//Buffer to read file into
+					int filebytes;//Bytes read in from file (should equal filesize)
+					filebytes = fread(filedata, 1, filesize, curfile);//Read file into buffer
+					char sizebuf[sizeof(int)];
+					sprintf(sizebuf, "%d", filesize);//Turn filesize into char array
+					char* med2;//Middleman to parse file type
+					char* pathtmp = new char [path.length()+1];
+					strcpy(pathtmp, path.c_str());//Turn path into a char array
+					strtok(pathtmp, ".");//Parse path delimited by .
+					char *med2tmp;
+					while ((med2tmp=strtok(NULL, "."))!=NULL){//Read until the last '.'
+						med2 = med2tmp;//Get the file extension
+					}
+					int ans=-1;
+					for (int i=0;i<7;i++){
+						if (!strcmp(med2, types[i])) ans=i;//Find the file type from the hard-coded list on line 15
+					}
+					if (ans==-1){
+						cout<<"ERROR: file type not recognized: "<<med2<<endl;
+						char fileout[5000] = "HTTP/1.1 500 Internal Server Error";
+						write(csock, fileout, strlen(fileout));
+						close(csock);
+						free(messagein);
+						return 1;
+					}
+					char *filehead = (char*)malloc(500);//Malloc buffer for the file header
+					strcpy(filehead, "HTTP/1.1 200 Document Follows\r\nContent-Type: ");
+					strcat(filehead, corrosponding[ans]);//Set Content-Type to matching field from the list on line 16
+					strcat(filehead, "\r\nContent-Length: ");
+					strcat(filehead, sizebuf);
+					strcat(filehead, "\r\n\r\n");
+					int headersize = strlen(filehead); //Size in bytes of the header
+					char send[filebytes+headersize]; //Final buffer for the message out
+					memcpy(send, filehead, headersize);//Copy the header to the beginning of the buffer (this is to avoid NULL char issues with strcpy)
+					memcpy(send+headersize, filedata, filebytes);//Copy the file data to the location right after the header
+					write(csock, send, filebytes+headersize);//Send the data
+					free(filedata);
+					free(filehead);
+					shutdown(csock, SHUT_WR);//Tell client we want to close the connection
+					while (recv(csock, messagein, 100000, 0)!=0){}//Wait for them to be ready
+					close(csock);//Close the connection
+					free(messagein);
+					return 1;
+				}else{
+					curfile = fopen("cache/"+urlhash, wb);
+					int ssock, msgsize;
+					struct sockaddr_in server;
+					struct hostent *sent;
+					memset(&server, 0, sizeof(server));
+					server.sin_family = AF_INET;
+					/* the following code block is from echoClient.c from Assignment1
+				    /* Map port number (char string) to port number (int)*/
+        			if ((server.sin_port=htons((unsigned short)atoi(80))) == 0)
+                		errexit("can't get \"%s\" port number\n", 80);
+
+    				/* Map host name to IP address, allowing for dotted decimal */
+        			if ( sent = gethostbyname(host) )
+                		memcpy(&server.sin_addr, sent->h_addr, sent->h_length);
+        			else if ( (server.sin_addr.s_addr = inet_addr(host)) == INADDR_NONE )
+                		errexit("can't get \"%s\" host entry\n", host);
+
+    				/* Allocate a socket */
+        			s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        			if (s < 0)
+                		errexit("can't create socket: %s\n", strerror(errno));
+
+    				/* Connect the socket */
+        			if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+            			errexit("can't connect to %s.%s: %s\n", host, portnum, strerror(errno));
+
+            		write(ssock, messagein, sizeof(messagein));
+            		recv(ssock, messagein, sizeof)
+    				if (fseek(curfile, 0, SEEK_END) != 0) perror("ERROR: Fseek failed");
+					filesize = ftello(curfile);//Seek to end of file and report position to get file size
+					rewind(curfile);//Reset file location pointer
+					char* filedata = (char*)malloc(filesize);//Buffer to read file into
+					int filebytes;//Bytes read in from file (should equal filesize)
+					filebytes = fread(filedata, 1, filesize, curfile);//Read file into buffer
+					char sizebuf[sizeof(int)];
+					sprintf(sizebuf, "%d", filesize);//Turn filesize into char array
+					char* med2;//Middleman to parse file type
+					char* pathtmp = new char [path.length()+1];
+					strcpy(pathtmp, path.c_str());//Turn path into a char array
+					strtok(pathtmp, ".");//Parse path delimited by .
+					char *med2tmp;
+					while ((med2tmp=strtok(NULL, "."))!=NULL){//Read until the last '.'
+						med2 = med2tmp;//Get the file extension
+					}
+					int ans=-1;
+					for (int i=0;i<7;i++){
+						if (!strcmp(med2, types[i])) ans=i;//Find the file type from the hard-coded list on line 15
+					}
+					if (ans==-1){
+						cout<<"ERROR: file type not recognized: "<<med2<<endl;
+						char fileout[5000] = "HTTP/1.1 500 Internal Server Error";
+						write(csock, fileout, strlen(fileout));
+						close(csock);
+						free(messagein);
+						return 1;
+					}
+					char *filehead = (char*)malloc(500);//Malloc buffer for the file header
+					strcpy(filehead, "HTTP/1.1 200 Document Follows\r\nContent-Type: ");
+					strcat(filehead, corrosponding[ans]);//Set Content-Type to matching field from the list on line 16
+					strcat(filehead, "\r\nContent-Length: ");
+					strcat(filehead, sizebuf);
+					strcat(filehead, "\r\n\r\n");
+					int headersize = strlen(filehead); //Size in bytes of the header
+					char send[filebytes+headersize]; //Final buffer for the message out
+					memcpy(send, filehead, headersize);//Copy the header to the beginning of the buffer (this is to avoid NULL char issues with strcpy)
+					memcpy(send+headersize, filedata, filebytes);//Copy the file data to the location right after the header
+					write(csock, send, filebytes+headersize);//Send the data
+					free(filedata);
+					free(filehead);
+					shutdown(csock, SHUT_WR);//Tell client we want to close the connection
+					while (recv(csock, messagein, 100000, 0)!=0){}//Wait for them to be ready
+					close(csock);//Close the connection
+					free(messagein);
+					return 1;
+				}
 				if (path=="/" || path=="/inside/"){//If we need to send index.html
 					curfile = fopen("./www/index.html", "rb");//Open index in read binary mode (for NULL characters)
 					if (curfile!=NULL){//If file is open
@@ -185,5 +307,4 @@ int main(int argc, char* argv[]){
 			return 0;
 		}
 	}
-	*/
 }
