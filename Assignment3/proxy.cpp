4ddc69edb78016716b83c62111f853e5b2a53dea
MugBergerFries <sjamesberger@gmail.com>
2018-11-20 02:06:45 -0700

    s

--- Assignment3/proxy.cpp
+++ Assignment3/proxy.cpp
@@ -191,7 +191,7 @@ int main(int argc, char* argv[]){
         			int test;
        				cout<<"sending bytes: "<<strlen(messagecpy)<<endl;
             		test = write(ssock, messagecpy, strlen(messagecpy));
-            		cout<<"test is "<<path.substr(path.substr(11).find('/', 0))<<endl;
+            		cout<<"test is "<<path.substr(11).find('/', 0)<<endl;
             		string final = ("GET " + path.substr(path.substr(11).find("/", 0)) + " " + vers + "\r\n\r\n");
             		cout<<final<<" is final"<<endl;
             		while((msgsize = recv(ssock, messagecpy, 100000, 0)) > 0){
