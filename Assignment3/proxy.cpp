894281540e989815039c9d628e201059ba5f02d2
MugBergerFries <sjamesberger@gmail.com>
2018-11-20 02:59:17 -0700

    asd

--- Assignment3/proxy.cpp
+++ Assignment3/proxy.cpp
@@ -77,15 +77,13 @@ int main(int argc, char* argv[]){
 			memcpy(messagecpy, messagein, 100000);
 			char* med;//Middleman to parse message
 			med = strtok(messagein, " ");//Get message until first " "
-			string method, path, vers, opt;
+			string method, path, vers;
 			method = med;//In this case, this will be GET, POST, HEAD
 			med = strtok(NULL, " ");//Get after first space and before second
 			path = med;//In this case, this will be the path requested
 			med = strtok(NULL, " ");
 			vers = med;//This will be the HTTP version
-			med = strtok(NULL, "\0");
-			opt = med;//Everything left
-			cout<<"opt is "<<opt;
+			cout<<"vers is "<<vers;
 			vector<string>::iterator urlindex;
 			string urlhash = to_string(f(path));
 			if (method=="GET"){
@@ -192,7 +190,7 @@ int main(int argc, char* argv[]){
             			exit(1);
         			}
         			int test;
-            		string final = ("GET " + path.substr(path.substr(11).find("/", 0)+11) + " " + vers + opt);
+            		string final = ("GET " + path.substr(path.substr(11).find("/", 0)+11) + " " + vers + "\r\n\r\n");
             		test = write(ssock, final.c_str(), strlen(final.c_str()));
             		while((msgsize = recv(ssock, messagecpy, 100000, 0)) > 0){
             			cout<<"Reply received: size = "<<msgsize<<endl;
